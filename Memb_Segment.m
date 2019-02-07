33e32825fc1d07f67b071efee27ce9d581918304
FRETcounter <46800680+FRETcounter@users.noreply.github.com>
2019-02-07 15:15:30 +0900

    Update Memb_Segment.m

--- Memb_Segment.m
+++ Memb_Segment.m
@@ -2,7 +2,7 @@
 function Output = Memb_Segment(ROI_Memb_Gray,   BW_Mask, SEG_Method,...
     Current_Seed, BlockProcessingHight, BlockProcessingWidth, ...
     Current_Minimum_Seed, Manually_Corrected_Membrane, Multithresh_Num_of_Clusters, ...
-    Mask_Dilation, Memb_Constuct_Cluster)
+    Mask_Dilation, Memb_Constuct_Cluster, N_memb, Subfolder_path_and_name)
 
 
 
@@ -68,14 +68,14 @@ switch SEG_Method
         
         Output = bwmorph(put_seed, 'thin', Inf);   %Final Output
         
-        
-        
-        
-        
+
     case{6}   %Trace based
         
         Manually_Corrected_Membrane = Manually_Corrected_Membrane;  %Manually_Corrected_Membrane of the previous frame
         ROI_Memb_Gray = ROI_Memb_Gray;
+        
+       %imwrite(ROI_Memb_Gray, [Subfolder_path_and_name,['\Memb_', N_memb]], 'tif', 'Compression','none');   %XXXXXX
+        
         [rows,cols] = size(Manually_Corrected_Membrane);
         Memb_Significant_Pixels = zeros(size(Manually_Corrected_Membrane));
         Sum_Significant_Pixels = im2bw(zeros(size(Manually_Corrected_Membrane)));
@@ -87,37 +87,47 @@ switch SEG_Method
                         Memb_Significant_Pixels(row, col) = 1;
                         BW_Significant = im2bw(Memb_Significant_Pixels);
                         
-                        SE = strel('rectangle',[Mask_Dilation Mask_Dilation]);     %Thickness level of the mask input
+                        SE = strel('rectangle',[Mask_Dilation   Mask_Dilation]);     %Thickness level of the mask input
                         BW_Significant_Block = imdilate(BW_Significant, SE);
                         Gray_Significant_Block = immultiply(BW_Significant_Block, ROI_Memb_Gray);
                         
-                        %Get the highest 2 clusters of four
                         
-                        Num_Clust = Multithresh_Num_of_Clusters;
-                        thresh = multithresh(Gray_Significant_Block, Num_Clust-1);
-                        seg_I = imquantize(Gray_Significant_Block, thresh);
-                        RGB = label2rgb(seg_I);
-                        ind = [];
-                        Cluster_array = [];
-                        
-                        for c = 1:Num_Clust
-                            Obj = seg_I==c;
-                            ind(c, 1) = nnz(Obj);
-                            %figure, imshow(Obj);
+                        if Mask_Dilation>=3  % in case we have a block contains more than three pixels
                             
-                            Cluster_array{c, 1} = Obj;
+                            %Get the highest 2 clusters of four
+                            Num_Clust = Multithresh_Num_of_Clusters;
+                            thresh = multithresh(Gray_Significant_Block, Num_Clust-1);
+                            seg_I = imquantize(Gray_Significant_Block, thresh);
+                            RGB = label2rgb(seg_I);
+                            ind = [];
+                            Cluster_array = [];
+                            
+                            for c = 1:Num_Clust
+                                Obj = seg_I==c;
+                                ind(c, 1) = nnz(Obj);
+                                %figure, imshow(Obj);
+                                
+                                Cluster_array{c, 1} = Obj;
+                            end
+                            
+                            Last_Cluster =(Cluster_array{Num_Clust,1});
+                            Second_Last_Cluster =(Cluster_array{(Num_Clust-1),1});
+                            
+                            if Memb_Constuct_Cluster ==1
+                                Both_Clust = imadd(Last_Cluster, Last_Cluster);
+                            elseif Memb_Constuct_Cluster ==2
+                                Both_Clust = imadd(Last_Cluster, Second_Last_Cluster);
+                            end
+                            
+                            out = imbinarize(Both_Clust);
+                            
+                            
+                        elseif Mask_Dilation <3 % in case we have a block contains less than three pixels
+                            
+                            
+                            out = imbinarize(Gray_Significant_Block);
+                            
                         end
-                        
-                        Last_Cluster =(Cluster_array{Num_Clust,1});
-                        Second_Last_Cluster =(Cluster_array{(Num_Clust-1),1});
-                        
-                        if Memb_Constuct_Cluster ==1
-                            Both_Clust = imadd(Last_Cluster, Last_Cluster);
-                        elseif Memb_Constuct_Cluster ==2
-                            Both_Clust = imadd(Last_Cluster, Second_Last_Cluster);
-                        end
-                        
-                        out = im2bw(Both_Clust);
                         
                         
                         %****************************
@@ -127,7 +137,7 @@ switch SEG_Method
                         %*****************************
                         
                         Sum_Significant_Pixels = imadd(out, Sum_Significant_Pixels);  % Cummulative sum
-                        Sum_Significant_Pixels = im2bw(Sum_Significant_Pixels);
+                        Sum_Significant_Pixels = imbinarize(Sum_Significant_Pixels);
                         Memb_Significant_Pixels = zeros(size(Manually_Corrected_Membrane));
                         
                     catch
@@ -138,7 +148,8 @@ switch SEG_Method
             end
         end
         bw = ~Sum_Significant_Pixels;  %membrane in black
-        
+
+ 
         %Watershed set
         level = 1;
         D = -bwdist(~bw);
@@ -147,26 +158,34 @@ switch SEG_Method
         Ld2 = watershed(D2);
         II = Ld2;
         II(II > 0) = 1;
-        
-        %Now smooth the segmented membrane generated from watershed by:
-        % 1- Dilation of 3x3
-        % 2- put the minimum seed. Because small cell might close when applying step 1
-        % 3- apply thinning inf
-        
-        % Binarize the membrane because it is not logical yet
         thr = graythresh(II);
         II = imbinarize(II,thr);
         II = ~II;
+        %Now smooth the segmented membrane generated from watershed by:
+        % 0- thinning to inf remove the sharpness caused by watershed
+        % 1- Dilation of 3x3
+        % 2- put the minimum seed. Because small cell might disappear when applying step 1
+        % 3- apply thinning to inf
+        
+        % Binarize the membrane because it is not logical yet
+        
+        Thin_II = bwmorph(II, 'thin', Inf);
+
         Smooth_Dilation_Size = 3;
         SE_Wide = strel('rectangle', [Smooth_Dilation_Size   Smooth_Dilation_Size]);
-        I_Dilate = imdilate(II, SE_Wide);
+        I_Dilate = imdilate(Thin_II, SE_Wide);
         
+  
         % now put the minimum seed
         put_seed = immultiply(I_Dilate, ~Current_Minimum_Seed);
         put_seed = im2bw(put_seed);
         
+         
+        %I_Erode = imdilate(put_seed, SE_Wide);
+        
         Output = bwmorph(put_seed, 'thin', Inf);   %Final Output result image
         
+        imwrite(Current_Minimum_Seed, [Subfolder_path_and_name,['\Seed_', N_memb]], 'tif', 'Compression','none');  
         
 end
 
